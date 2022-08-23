d85a989aef34dd71848c6d559f3874b62348ac88
Younghan Kim <kimyhan0222@gmail.com>
2022-08-23 15:53:36 +0900

    second commit

--- chap2/src/chap2/Test1.java
+++ chap2/src/chap2/Test1.java
@@ -3,52 +3,53 @@ package chap2;
 public class Test1 {
 	public static void main(String[] args) {
      //1. 잘못된 문장을 골라내고 그 이유를 설명하시오.		
-	    char       a='a'; 
-//	    char       b="a";    // 작은따옴표를 사용해야함.
-	    String     c="a";   
-//	    String     d='a';	 // 큰따옴표를 사용해야함.
-//	    char       e='ab'; 	 //
-	    String     f="ab"; 
-//	    char       g ='';	 // '' 안에 공백 불가
-	    String     h = ""; 
+        char       a='a';   //정상
+//	    char       b="a";   //오류. char <- String 저장 
+	    String     c="a";   //정상
+//	    String     d='a';   //오류  String <- char 저장
+//	    char       e='ab';  //오류. char는 한개문자만 저장가능
+	    String     f="ab"; //정상 
+//	    char       g = ''; //오류.char는 한개문자만 저장가능.없어도안됨
+	    String     h = ""; //정상. 빈문자열 가능. 
 
-//	    byte   var1 = 128; 	// byte : -128 ~ 127
-		short  var2 = 128; 
-//		int    var3 = 28L;	// L : long형 리터널 이므로 28L : long임
-		long   var4 = 128L;
-//		float  var5 = 1.2;  	//	리터널 기본형이 double이므로 float형 리터널을 붙여야함. 
-		double var6 = 1.2; 
-	    float var9 = '1';   
-	    
-	    int defalut = 1;
-	    
-	    System.out.println(var9);
+//	    byte   var1 = 128; //오류 -128 ~ 127까지 저장가능.범위밖 
+//	    short   var1 = 128; //var1 변수의 자료형을 저장 가능한 자료형으로 변경 
+	    short   var1 = (byte)128; //형변환 
+		short  var2 = 128;  //정상 
+//		int    var3 = 28L;  // int <- long. 형변환 연산자 필수
+		int    var3 = (int)28L;  // int <- long. 형변환 연산자 필수
+		long   var4 = 128L; //정상
+//		float  var5 = 1.2;  //실수형리터널의 기본 자료형 double
+		float  var5 = 1.2f;  //실수형리터널의 기본 자료형 double
+		double var6 = 1.2;  //정상 
+	    float var9 = '1'; //정상. float<-char타입의 코드값이 실수형
+	    System.out.println("var1="+var1); //-128
 	}
 }
 /*
 2.   변수를 선언하기 위해서는 자료형과 변수의 이름을 결정해야합니다. 
       변수 이름으로 사용할 수 있는 것을 고르시오.
       변수이름으로 사용할 수 없는 이유를 작성하시오
-      A. false	 (X) : 예약어
-      B. default (O) : 예약어 아님
-      C. _var	 (O) : _ 특수문자 사용가능
-      D. a-class (X) : - 사용불가
+      A. false    : 예약어 변수 사용 불가
+      B. default  : 예약어 변수 사용 불가
+      C. _var     : 변수 사용 가능
+      D. a-class  : - 문자는 변수 사용 불가
       
 3. 변수를 선언하기 위해서는 자료형과 변수의 이름을 결정해야합니다.
    변수 이름으로 사용할 수 있는 것을 고르시오.
-    A. s#arp  
-    B. static  
-    C. _class (O)
-    D. class 
+    A. s#arp  : #문자는 변수 사용 불가  
+    B. static : 예약어 변수 사용 불가 
+    C. _class : 가능
+    D. class  : 예약어 변수 사용 불가
     
 4. byte 형을 저장할 수 있는 데이터 값의 허용 범위는 얼마입니까?
-     -128 ~ 127
+    -128 ~ 127 : 256가지 값 가능
+                 0 ~ 255 : 부호때문에 -128 ~ 127까지 범위.       
  
 5. float 형 변수 선언이 올바르게 된 것을 모두 고르시오.
-   A. float f1 = 1F;	(O)
-   B. float f2 = 1.0; 	(O)
-   C. float f3 = '1';  
-   D. float f4 = "1";  
-   E. float f5 = 1.0d;      
+   A. float f1 = 1F;   //정상 
+   B. float f2 = 1.0;  //오류. 1.0 double 형. 형변환,접미사
+   C. float f3 = '1';  //정상. float <- char
+   D. float f4 = "1";  //오류. float(기본형) <- String(참조형)
+   E. float f5 = 1.0d; //오류. 형변환연산자 필요.      
 */
-
--- chap2/src/chap2/Test2.java
+++ chap2/src/chap2/Test2.java
@@ -7,24 +7,33 @@ ch=C
 f=1.5
 l=27000000000
 result=true 
+c=30
+ch=C
+f=1.0
+l=1230196224
+result=false
 */
 public class Test2 {
 	public static void main(String[] args) {
 		   byte a = 10;
 		   byte b = 20;
-		   int c = a + b; 
+		   byte c = (byte)(a + b); // 연산의 결과 int
 		   char ch = 'A';
-		   ch = (char) (ch + 2);
-		   float f1 = 3F;
-		   float f = f1 / 2;
-		   long l = 3000L * 3000L * 3000L;
+		   ch = (char)(ch + 2); // 연산의 결과 int
+		   float f = 3f / 2;    // float <-float=float/int
+		   long l= 3000L * 3000 * 3000;
 		   float f2 = 0.1f;
 		   double d = 0.1;
-		   boolean result = (float)d==(f2);
+		   // 0.1 == 0.1f : 같은 값? 
+		   boolean result = (float)d==f2; //false
 		   System.out.println("c="+c);
 		   System.out.println("ch="+ch);
 		   System.out.println("f="+f);
 		   System.out.println("l="+l);
-		   System.out.println("result="+result);
+		   System.out.println("result="+result); //자바버그.
+		   //%.30f : 실수형은 소숫점이하 30자리로 출력
+		   System.out.printf("%.30f, %.30f\n",f2,d);
+		   System.out.println(f2); //0.1
+		   System.out.println(d);  //0.1
 	}
 }
